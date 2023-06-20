daf79b49176b24190e7228e631f4e6bb68b8a5d1
thaisrocha <rochathais.ml@gmail.com>
2023-06-20 20:32:08 -0300

    Adicionando comentário do meu estudo

--- 1.filtrando-IP.sh
+++ 1.filtrando-IP.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #Descricao: O script acessa o arquivo apache-log e busca pelo IP passado por parametro e tras o log.
 
-
+# Guardando o path em variavel
 CAMINHO_LOG=/var/log/apache2
+# Entrando no diretório do path
 cd $CAMINHO_LOG
-
+# Usando o regex para validar o parametro que será passado quando executar o script, armazendo na variavel REGEX
 REGEX="\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b"
-
-if [[ $1 =~ $REGEX ]] # =~ e comparando se e igual, neste caso usa dois [[]] pois estamos usando comparacao com o regex dentro da condicao.
+# IF de condição validando se o parametro passado é igual ao regex.
+if [[ $1 =~ $REGEX ]] # neste caso usa dois [[]] pois estamos usando comparacao com o regex dentro da condicao.
 then
-    cat access.log | grep $1  # $1 pega o primeiro passado quando chama o script.
-    if [ $? en 0 ] # Validando o status de saida do cat $?, en (Nao e igual - not equals), comparando com 0 pois todos status de comando retorna um valor, o de sucesso e 0.
+    cat access.log | grep $1  # busca e lista toda requisição do parametro passado dentro do arquivo acess.log 
+    if [ $? en 0 ] # Validando o status de saida do cat $?, en (Nao e igual - not equals) a 0, pois todos status de comando retorna um valor, o de sucesso é 0.
     then
+        # Dando um retorno ao usuário caso não encontre esse IP
         echo "IP nao encontrado no arquivo!"
     fi
 else
+    # Dando um retorno ao usuário caso o regex valide que o formato digitado não seja valido.
     echo "Formato de IP invalido!"
 fi
